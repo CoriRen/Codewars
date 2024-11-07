e51f13f25e5e0c07749129dacc3bb33a7188cb49
Corinne GoodSmith <corinnegoodsmith@gmail.com>
2024-11-06 22:33:06 -0600

    added daily challenge

---
+++ Count_positives_sum_negatives.js
@@ -0,0 +1,24 @@
+/*Given an array of integers.
+
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+If the input is an empty array or is null, return an empty array.
+
+Example
+For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].*/
+
+function countPositivesSumNegatives(input) {
+    let countOfPositives = 0;
+    let sumOfNegatives = 0;
+    
+    input.forEach((num) => {
+      if (num > 0) countOfPositives+=1;
+      if (num < 0) sumOfNegatives += num;
+    })
+  
+    let newArray=[countOfPositives,sumOfNegatives]
+  
+    return newArray
+  }
+  
+  
