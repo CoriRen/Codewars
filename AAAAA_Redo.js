0b8f6bcc156349a3646b88494ecd0ac45a7d0ba8
Corinne GoodSmith <corinnegoodsmith@gmail.com>
2024-12-17 12:29:53 -0600

    added daily challenge

--- AAAAA_Redo.js
+++ AAAAA_Redo.js
@@ -76,3 +76,17 @@ all((1, 2, 3, 4, 5), less_than_9)    -> True */
 function all( arr, fun ){
   return arr.every(fun)
 }
+
+/**Write a function that takes an array of strings as an argument and returns a sorted array containing the same strings, ordered from shortest to longest.
+
+For example, if this array were passed as an argument:
+
+["Telescopes", "Glasses", "Eyes", "Monocles"]
+Your function would return the following array:
+
+["Eyes", "Glasses", "Monocles", "Telescopes"]
+All of the strings in the array passed to your function will be different lengths, so you will not have to decide how to order multiple strings of the same length. */
+
+function sortStrs (array){
+  return array.sort((a,b) => a.length-b.length)
+}
